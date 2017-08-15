1561b57516fb74813ded9ec299cfaea5bf70f972
Andrew Kabatznick <amkabatznick@gmail.com>
2017-08-14 23:11:32 -0400

    Updated create_database.py

--- create_database.py
+++ create_database.py
@@ -74,7 +74,7 @@ conn.commit()
 Sections = 'home, arts, automobiles, books, business, fashion, food, health, insider, magazine, movies, national, nyregion, obituaries, opinion, politics, realestate, science, sports, sundayreview, technology, theater, tmagazine, travel, upshot, world'
 SectionString = _cleanSections(Sections)
 
-cur.executemany("Insert into sections (section_name) Values %s;", (SectionString,) )
+cur.executemany("Insert into sections (section_name) Values (%s);", (SectionString,) )
 conn.commit()
 
 conn.close()
