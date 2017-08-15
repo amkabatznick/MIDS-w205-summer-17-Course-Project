0349c727ce794dd49054aac2110faf93f3eecdf9
Andrew Kabatznick <amkabatznick@gmail.com>
2017-08-15 19:10:04 -0400

    Updated API Parser

--- NYT_API_Parser.py
+++ NYT_API_Parser.py
@@ -73,5 +73,4 @@ for section in sections:
                     for facet in i[j]:
                         facet_details_id = _return_field_details(conn,facet,'facet_details')
                         cur.execute("INSERT INTO article_facet_details Values ('article_id','facet_id','facet_detail_id')",(article_id,facet_type_id,facet_details_id))
-
-            conn.commit()
+        conn.commit()
