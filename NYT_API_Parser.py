9a6f2bba3a7d214d26c99594d8ebdb3036dad7f3
Andrew Kabatznick <amkabatznick@gmail.com>
2017-08-15 18:58:28 -0400

    Updated API Parser

--- NYT_API_Parser.py
+++ NYT_API_Parser.py
@@ -43,39 +43,39 @@ for section in sections:
     r= requests.get('http://api.nytimes.com/svc/topstories/v2/%s.json?api-key=%s' %(section_name,NYTimesApi)).json()
 
     for i in r['results']:
-    url = i['url']
-    title = i['title']
-    update_date = i['updated_date'][0:10]+' '+ i['updated_date'][11:19]
-    section_id
-    subsection = i['subsection']
-    sub_section_id = _retn_field_details(conn,subsection,'facet_details')
+        url = i['url']
+        title = i['title']
+        update_date = i['updated_date'][0:10]+' '+ i['updated_date'][11:19]
+        section_id
+        subsection = i['subsection']
+        sub_section_id = _retn_field_details(conn,subsection,'facet_details')
 
-    cur.execute("INSERT INTO article_details Values ('title','url','update_date','section_id','sub_section_id') RETURNING 'article_id'",
-        (title,url, update_date,section_id, sub_section_id))
-    article_id = cur.fetchone()[0]
+        cur.execute("INSERT INTO article_details Values ('title','url','update_date','section_id','sub_section_id') RETURNING 'article_id'",
+            (title,url, update_date,section_id, sub_section_id))
+        article_id = cur.fetchone()[0]
 
-        for j in i.keys():
-          #Check if this is a facet
-          if 'facet' in j:
-              #Get this FacetId
-              facet_type_id = _return_field_details(conn, j,'facet_types')
+            for j in i.keys():
+              #Check if this is a facet
+              if 'facet' in j:
+                  #Get this FacetId
+                  facet_type_id = _return_field_details(conn, j,'facet_types')
 
-              #See if this facet has information
-              if i[j]:
-                #If its a person update this information
-                if j == 'per_facet':
-                    for per in i['per_facet']:
-                        per = per.split(',')
-                        name = per[1].split()[0]+' '+per[0]ur
-                        facet_details_id = _return_field_details(conn,name,'facet_details'))
-                        cur.execute("INSERT INTO article_facet_details Values
+                  #See if this facet has information
+                  if i[j]:
+                    #If its a person update this information
+                    if j == 'per_facet':
+                        for per in i['per_facet']:
+                            per = per.split(',')
+                            name = per[1].split()[0]+' '+per[0]ur
+                            facet_details_id = _return_field_details(conn,name,'facet_details'))
+                            cur.execute("INSERT INTO article_facet_details Values
+                                ('article_id','facet_id','facet_detail_id')",
+                                (article_id,facet_type_id,facet_details_id))
+                    else:
+                        for facet in i[j]:
+                            facet_details_id = _return_field_details(conn,facet,'facet_details'))
+                            cur.execute("INSERT INTO article_facet_details Values
                             ('article_id','facet_id','facet_detail_id')",
                             (article_id,facet_type_id,facet_details_id))
-                else:
-                    for facet in i[j]:
-                        facet_details_id = _return_field_details(conn,facet,'facet_details'))
-                        cur.execute("INSERT INTO article_facet_details Values
-                        ('article_id','facet_id','facet_detail_id')",
-                        (article_id,facet_type_id,facet_details_id))
 
-        conn.commit()
+            conn.commit()
