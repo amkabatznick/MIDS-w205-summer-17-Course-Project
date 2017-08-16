324ae1d289e60ba59854039b1e1f62b1467fd167
Andrew Kabatznick <amkabatznick@gmail.com>
2017-08-15 22:50:44 -0400

    Updated API Parser

--- NYT_API_Parser.py
+++ NYT_API_Parser.py
@@ -26,9 +26,6 @@ def _return_field_details(conn, Value,Table):
                     }
     cur.execute(TableMapping[Table]['GetSql'], (Value,))
     if not cur.rowcount:
-        if Table == 'facet_details':
-            print(cur.query)
-            print(cur.rowcount)
         cur.execute(TableMapping[Table]['InsertSql'], (Value,))
         conn.commit()
 
@@ -53,41 +50,43 @@ for section in sections:
     r= requests.get('http://api.nytimes.com/svc/topstories/v2/%s.json?api-key=%s' %(section_name,NYTimesApi)).json()
 
     for i in r['results']:
-        url = i['url']
         title = i['title']
-        update_date = i['updated_date'][0:10]+' '+ i['updated_date'][11:19]
-        if i['subsection']:
-            subsection = i['subsection']
-            sub_section_id = _return_field_details(conn,subsection,'subsections')
+        cur.execute("SELECT article_id from article_details where title=%s", (title,))
+        if not cur.rowcount:
+            url = i['url']
+            update_date = i['updated_date'][0:10]+' '+ i['updated_date'][11:19]
+            if i['subsection']:
+                subsection = i['subsection']
+                sub_section_id = _return_field_details(conn,subsection,'subsections')
 
-            cur.execute("INSERT INTO article_details (title,url,update_date,section_id,sub_section_id) Values(%s,%s,%s,%s,%s)  RETURNING article_id",
-                (title,url, update_date,section_id, sub_section_id))
-        else:
-            cur.execute("INSERT INTO article_details (title,url,update_date,section_id) Values (%s,%s,%s,%s) RETURNING article_id",
-                (title,url, update_date,section_id))
+                cur.execute("INSERT INTO article_details (title,url,update_date,section_id,sub_section_id) Values(%s,%s,%s,%s,%s) ON CONFLICT UPDATE RETURNING article_id",
+                    (title,url, update_date,section_id, sub_section_id))
+            else:
+                cur.execute("INSERT INTO article_details (title,url,update_date,section_id) Values (%s,%s,%s,%s) RETURNING article_id",
+                    (title,url, update_date,section_id))
 
-        article_id = cur.fetchone()[0]
-        for j in i.keys():
-          #Check if this is a facet
-          if 'facet' in j:
-              #Get this FacetId
-              facet_name = j
-              facet_type_id = _return_field_details(conn, facet_name,'facet_types')
+            article_id = cur.fetchone()[0]
+            for j in i.keys():
+              #Check if this is a facet
+              if 'facet' in j:
+                  #Get this FacetId
+                  facet_name = j
+                  facet_type_id = _return_field_details(conn, facet_name,'facet_types')
 
-              #See if this facet has information
-              if i[j]:
-                #If its a person update this information
-                if j == 'per_facet':
-                    for per in i['per_facet']:
-                        per = per.split(',')
-                        if len(per) > 1:
-                            name = per[1].split()[0]+' '+per[0]
-                        else:
-                            name = per[0]
-                        facet_details_id = _return_field_details(conn,name,'facet_details')
-                        cur.execute("INSERT INTO article_facet_details (article_id,facet_id,facet_detail_id) Values(%s,%s,%s)",(article_id,facet_type_id,facet_details_id,))
-                else:
-                    for facet in i[j]:
-                        facet_details_id = _return_field_details(conn,facet,'facet_details')
-                        cur.execute("INSERT INTO article_facet_details (article_id,facet_id, facet_detail_id) Values(%s,%s,%s)",(article_id,facet_type_id,facet_details_id,))
-        #conn.commit()
+                  #See if this facet has information
+                  if i[j]:
+                    #If its a person update this information
+                    if j == 'per_facet':
+                        for per in i['per_facet']:
+                            per = per.split(',')
+                            if len(per) > 1:
+                                name = per[1].split()[0]+' '+per[0]
+                            else:
+                                name = per[0]
+                            facet_details_id = _return_field_details(conn,name,'facet_details')
+                            cur.execute("INSERT INTO article_facet_details (article_id,facet_id,facet_detail_id) Values(%s,%s,%s)",(article_id,facet_type_id,facet_details_id,))
+                    else:
+                        for facet in i[j]:
+                            facet_details_id = _return_field_details(conn,facet,'facet_details')
+                            cur.execute("INSERT INTO article_facet_details (article_id,facet_id, facet_detail_id) Values(%s,%s,%s)",(article_id,facet_type_id,facet_details_id,))
+            #conn.commit()
