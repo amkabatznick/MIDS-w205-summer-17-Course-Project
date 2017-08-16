b483b23658deef63dcf59a6c21cf9fcc7b26d097
Andrew Kabatznick <amkabatznick@gmail.com>
2017-08-15 23:25:46 -0400

    Updated API Parser

--- NYT_API_Parser.py
+++ NYT_API_Parser.py
@@ -84,12 +84,13 @@ for section in sections:
                             else:
                                 name = per[0]
                             facet_details_id = _return_field_details(conn,name,'facet_details')
-                            cur.execute("SELECT id from rticle_facet_details where article_id=%s and facet_id=%s and facet_detail_id=%s", (article_id,facet_type_id,facet_details_id,)
-                            if
-                            else:
+                            cur.execute("SELECT id from article_facet_details where article_id=%s and facet_id=%s and facet_detail_id=%s", (article_id,facet_type_id,facet_details_id,)
+                            if not cur.rowcount:
                                 cur.execute("INSERT INTO article_facet_details (article_id,facet_id,facet_detail_id) Values(%s,%s,%s)",(article_id,facet_type_id,facet_details_id,))
                     else:
                         for facet in set(i[j]):
                             facet_details_id = _return_field_details(conn,facet,'facet_details')
-                            cur.execute("INSERT INTO article_facet_details (article_id,facet_id, facet_detail_id) Values(%s,%s,%s)",(article_id,facet_type_id,facet_details_id,))
-            #conn.commit()
+                            cur.execute("SELECT id from article_facet_details where article_id=%s and facet_id=%s and facet_detail_id=%s", (article_id,facet_type_id,facet_details_id,)
+                            if not cur.rowcount:
+                                cur.execute("INSERT INTO article_facet_details (article_id,facet_id,facet_detail_id) Values(%s,%s,%s)",(article_id,facet_type_id,facet_details_id,))
+        
