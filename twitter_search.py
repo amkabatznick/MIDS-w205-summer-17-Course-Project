4e9b5dcaf5ee93956b9b60b6843e56a84facd06a
claymiller11 <claymiller11@gmail.com>
2017-08-17 00:38:18 -0700

    Add files via upload

---
+++ twitter_search.py
@@ -0,0 +1,25 @@
+import tweepy
+import os
+import sys
+import json
+
+try:
+    consumer_key = os.environ['TWITTER_CONSUMER_KEY']
+    consumer_secret = os.environ['TWITTER_CONSUMER_SECRET']
+    access_token = os.environ['TWITTER_ACCESS_TOKEN']
+    access_token_secret = os.environ['TWITTER_ACCESS_SECRET']
+except KeyError:
+    sys.stderr.wirte("TWITTER_*environment variables not se\n")
+    sys.exit(1)
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+public_tweets = api.home_timeline()
+for tweet in public_tweets:
+    print tweet.text
+
+status_list = api.search('charlottesville')
+json.dumps([status._json for status in status_list)]
