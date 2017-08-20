ebdf8c9ec50cc991b08ff740038c7d28353c1dc3
Andrew Kabatznick <amkabatznick@gmail.com>
2017-08-20 15:06:04 -0400

    Adding test database connect

--- database_test.py
+++ database_test.py
@@ -4,8 +4,8 @@ import psycopg2
 conn = psycopg2.connect(database="postgres", user="postgres", password="pass", host="localhost", port="5432")
 
 cur = conn.cursor()
-cursor.execute("SELECT COUNT(*) = 0 FROM pg_catalog.pg_database WHERE datname = 'nyt'")
-not_exists_row = cursor.fetchone()
+cur.execute("SELECT COUNT(*) = 0 FROM pg_catalog.pg_database WHERE datname = 'nyt'")
+not_exists_row = cur.fetchone()
 not_exists = not_exists_row[0]
 if not_exists:
     print('Database does not exits')
